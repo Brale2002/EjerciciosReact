efc57817cfaad61fc3456d9119c2d6f8934b7944
BrayanMedinaMoreno <brayan0519@yahoo.com>
2023-03-03 10:22:58 -0500

    cambio de colores con useeffect

--- src/components/Contador/Contador.jsx
+++ src/components/Contador/Contador.jsx
@@ -1,14 +1,45 @@
 import '../../assets/css_styles/cuadro.css'
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import './counter.css'
 
 export const Contador = () => {
 
     const [counter, setCounter] = useState(0)
+    const [style, setStyle] = useState('red')
+    const [colorCaja, setColorCaja] = useState('azul')
+
+    useEffect(() =>{
+      if (counter >= 10) {
+        setStyle('green')
+      }else{
+        setStyle('red')
+      }
+    }, [counter])
+
+    useEffect(()=>{
+      let color = prompt("Ingrese un color: \n(azul, morado, verde)")
+      switch (color) {
+        case "verde":
+          setColorCaja('verde')
+          break;
+        case "morado":
+          setColorCaja('morado')
+          break
+        case "azul":
+          setColorCaja('azul')
+          break
+        default: alert("El color no esta en la lista")
+        setColorCaja('blanco')
+          break;
+      }
+    },[])
+
   return (
     <>
     <div className="contenidoCount">
+    <div className={colorCaja}></div>
 
-        <div className="cont_main">
+        <div className={style}>
             <h1>{counter}</h1> 
         </div>
 
@@ -16,6 +47,7 @@ export const Contador = () => {
             <button onClick={()=>setCounter(counter+1)}>Add</button>    
             <button onClick={()=>setCounter(0)}>Reset</button>    
             <button onClick={()=>setCounter(counter-1)}>Sudmit</button>    
+            <button onClick={()=>{location.reload()}}>cambiar color</button>    
         </div>   
 
     </div>
